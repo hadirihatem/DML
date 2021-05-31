a66c7c4f642ed7b3d83d5e9fa2f147ced6dd76f9
hatemhadiri <hatemhadiri9@gmail.com>
2021-05-31 15:29:46 +0100

    first commit

---
+++ DataMaipulationLanguage.sql
@@ -0,0 +1,9 @@
+-- Insert values in customers table
+INSERT INTO customers VALUES (‘C01’, ’Ali’, ‘71321009’)
+INSERT INTO customers VALUES (‘C02’, ’ASMA’, ‘77345823’)
+-- Insert values in product table
+INSERT INTO product VALUES (‘P01’, ‘Samsung Galaxy S20’, ‘Smartphone’, 3299)
+INSERT INTO product VALUES (‘P02’, ‘Asus Notebook’, ‘pc’, 4599)
+-- Insert values in orders t table
+INSERT INTO orders VALUES (‘C01‘, P02’, ‘Null’, 2, 9198)
+INSERT INTO orders VALUES (‘C02‘, P01’, ‘28/05/2020’, 1, 3299)
